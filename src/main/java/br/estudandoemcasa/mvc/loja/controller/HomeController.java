2941071b73375c6214bd14df615f7e062d0899a0
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-10-06 21:37:47 -0300

    Trabalhando com paginação

--- src/main/java/br/estudandoemcasa/mvc/loja/controller/HomeController.java
+++ src/main/java/br/estudandoemcasa/mvc/loja/controller/HomeController.java
@@ -4,11 +4,11 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.estudandoemcasa.mvc.loja.enums.StatusPedido;
@@ -24,7 +24,11 @@ public class HomeController {
 	
 	@GetMapping
 	public String home(Model request, Principal principal) {
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
+		
+		Sort sort = Sort.by("dataEntrega").ascending();
+		PageRequest pagination = PageRequest.of(0,1,sort);
+		
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, pagination);
 		request.addAttribute("pedidos", pedidos);
 		return "usuario/home";
 	}
